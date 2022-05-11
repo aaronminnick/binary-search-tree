eca2e7272d2ea7e51ce103a257c0d7d74add892d
Aaron Minnick <abminnick@gmail.com>
2022-05-11 14:21:01 -0700

    add tests and pass for adding nodes left, right, or duplicate

--- __tests__/bst.test.js
+++ __tests__/bst.test.js
@@ -11,7 +11,64 @@ describe('bst', () => {
   test('it should create a new root node', () => {
     let tree = new bst();
     let node = new bstnode(36);
-    tree.insertNode(node);
+    tree.insert(node);
     expect(tree.root).toEqual(node);
+  });
+  
+  test('it should add a child node to the left of the root node', () => {
+    let tree = new bst();
+    let rootNode = new bstnode(36);
+    tree.insert(rootNode);
+    let newNode = new bstnode(22);
+    tree.insert(newNode);
+    expect(rootNode.left.data).toEqual(22);
+  });
+
+  test('it should add a child node to the right of the root node', () => {
+    let tree = new bst();
+    let rootNode = new bstnode(36);
+    tree.insert(rootNode);
+    let newNode = new bstnode(48);
+    tree.insert(newNode);
+    expect(rootNode.right.data).toEqual(48);
+  });
+
+  test('it should add a child to the left of a child node', () => {
+    let tree = new bst();
+    let rootNode = new bstnode(36);
+    tree.insert(rootNode);
+    let node2 = new bstnode(22);
+    tree.insert(node2);
+    let node3 = new bstnode(11);
+    tree.insert(node3);
+    expect(rootNode.left.left.data).toEqual(11);
+  });
+
+  test('it should add a child to the right of a child node', () => {
+    let tree = new bst();
+    let rootNode = new bstnode(36);
+    tree.insert(rootNode);
+    let node2 = new bstnode(22);
+    tree.insert(node2);
+    let node3 = new bstnode(24);
+    tree.insert(node3);
+    expect(rootNode.left.right.data).toEqual(24);
+  });
+
+  test('it should not add duplicate nodes', () => {
+    let tree = new bst();
+    let rootNode = new bstnode(36);
+    tree.insert(rootNode);
+    let duplicateNode = new bstnode(36);
+    expect(tree.insert(duplicateNode)).toEqual(
+      {
+        "root": 
+        {
+          "data": 36, 
+          "left": null, 
+          "right": null
+        }
+      }
+    );
   });
 });
--- src/bst.js
+++ src/bst.js
@@ -3,7 +3,28 @@ export default class bst {
     this.root = null;
   }
 
-  insertNode(node) {
-    this.root = node;
+  insert(node) {
+    if (this.root == null) {
+      this.root = node;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (currentNode.data > node.data) {
+          if (currentNode.left == null) {
+            currentNode.left = node;
+            return this;
+          } else {
+            currentNode = currentNode.left;
+          }
+        } else if (currentNode.data < node.data) {
+          if (currentNode.right == null) {
+            currentNode.right = node;
+            return this;
+          } else {
+            currentNode = currentNode.right;
+          }
+        } else return this;
+      }
+    }
   }
 }
